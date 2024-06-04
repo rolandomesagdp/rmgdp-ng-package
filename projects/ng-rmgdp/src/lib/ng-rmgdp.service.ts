fd4771402e7dbdfe9eba5b200a8024ef34a4b80e
Rolando Mesa <rolandomesagdp@gmail.com>
2024-06-04 13:56:56 +0200

    Package publishable to github packages
    It was already published and it is available at https://github.com/rolandomesagdp/rmgdp-ng-package/pkgs/npm/ng-rmgdp

--- projects/ng-rmgdp/src/lib/ng-rmgdp.service.ts
+++ projects/ng-rmgdp/src/lib/ng-rmgdp.service.ts
@@ -6,4 +6,8 @@ import { Injectable } from '@angular/core';
 export class NgRmgdpService {
 
   constructor() { }
+
+  logMessage(message: string): void {
+    console.log(message);
+  }
 }
--- projects/ng-rmgdp/src/public-api.ts
+++ projects/ng-rmgdp/src/public-api.ts
@@ -1,6 +1,1 @@
-/*
- * Public API Surface of ng-rmgdp
- */
-
 export * from './lib/ng-rmgdp.service';
-export * from './lib/ng-rmgdp.component';
